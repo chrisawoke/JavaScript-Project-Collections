ea8e4ddccf50c061360ae5b9f4e34be4e159b6b1
Chris Awoke <uchechristian27@gmail.com>
2024-01-16 14:40:08 +0100

    Update slot.js

--- slot_machine/slot.js
+++ slot_machine/slot.js
@@ -10,13 +10,48 @@
 
 const prompt = require("prompt-sync")();
 
+// 1. Deposit some money (Done here ✅)
 const depositMoney = () => {
-  const depositAmount = prompt("Enter your deposit amount: ");
-  const numberDepositAmount = parseFloat(depositAmount);
+  while (true) {
+    const depositAmount = prompt("Enter your deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
 
-  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-    console.log("Invalid deposit amount. Please try again.");
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount. Please try again.");
+    } else {
+      return numberDepositAmount;
+    }
   }
 };
 
-depositMoney();
+// 2. Determine the number of lines to bet on (Done here ✅)
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines. Please try again.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+// 3. Collect a bet amount (Done here ✅)
+const getBet = (balance) => {
+  while (true) {
+    const bet = prompt("Enter the total bet amount: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance) {
+      console.log("Invalid bet amount. Please try again");
+    } else {
+      return numberBet;
+    }
+  }
+};
+
+let balance = depositMoney();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance);
