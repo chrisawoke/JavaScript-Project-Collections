26bc48cb000f76cc070405ebce1f9dc4a7f9f2ac
Chris Awoke <uchechristian27@gmail.com>
2024-01-16 14:17:58 +0100

    Update slot.js

--- slot_machine/slot.js
+++ slot_machine/slot.js
@@ -7,3 +7,16 @@
 // 5. Check if the user won
 // 6. Give the user their winnings or take their bet
 // 7. Play the game again
+
+const prompt = require("prompt-sync")();
+
+const depositMoney = () => {
+  const depositAmount = prompt("Enter your deposit amount: ");
+  const numberDepositAmount = parseFloat(depositAmount);
+
+  if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+    console.log("Invalid deposit amount. Please try again.");
+  }
+};
+
+depositMoney();
